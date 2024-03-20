f3266bcc1d0f1163baaf6cad798b4f7a5ce864b9
Gabriel Magarotto <gabriel@magarotto.com.br>
2024-03-20 13:53:19 +0000

    Função para verificar numeros primos separada da que exibe.

--- Logica.js
+++ Logica.js
@@ -1,27 +1,34 @@
-let canvas = document.getElementById("meuCanvas");
-let ctx = canvas.getContext('2d');
+var canvas = document.getElementById("meuCanvas");
+var ctx = canvas.getContext('2d');
 //  função que exibe oque foi digitado pelo usuario
 //  ( document.getElementById("Numero1").value ) usar isso para receber o valor digitado em alguma variavel
-function exibir() {
+function exibir(ctx) {
     ctx.clearRect(0, 0, 1000, 500); // limpa tela quando a funçao é chamada
-    let x = 0;                 // posição x em que sera escrito
-    let y = 250;                 // posição y em que sera escrito
-    let N1 = parseInt(document.getElementById("Numero1").value, 10);  // oque for digitado sera depositado na variavel recebe  
+    let x = 0;                 // posição x é a horizontal
+    let y = 250;                 // posição y  é a vertical
+    let Nd = parseInt(document.getElementById("Numero1").value, 10);  // oque for digitado sera depositado na variavel recebe  
     ctx.font = "100px Arial";
-    let resposta = N1 + " ";
-    let result = "é primo";
+
+    
+    var Vp[np];
+    // ctx.fillText(resultado, x, y);
+    primo();
+    
+}
+
+function primo(N1){
     let contador = 3;
+
     if (N1 < 2 || (N1 % 2 == 0 && N1 != 2)) {
-        result = "não é primo!";
+        return false;
     } else {
         while (contador <= Math.sqrt(N1)) {
             if (N1 % contador == 0) {
-                result = "não é primo!";
+                return false;
                 break;
             }
             contador = contador + 2;
         }
     }
-    let resultado = resposta + result;
-    ctx.fillText(resultado, x, y);
+    return true;
 }
